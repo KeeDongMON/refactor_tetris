65a6ce4a4fc76d2d7218096ab556f7752096abd6
doeeon <doun5824@gmail.com>
2025-05-19 11:55:25 +0900

    Update board.h

--- board.h
+++ board.h
@@ -30,9 +30,6 @@ void board_clear_data(cell_t board[BOARD_HEIGHT][BOARD_WIDTH]);
 void board_insert_block(cell_t board[BOARD_HEIGHT][BOARD_WIDTH], block_t* block, int block_x, int block_y);
 
 
-//void board_init(int data[BOARD_HEIGHT][BOARD_WIDTH]);
-//void board_display_info(cell_t board[BOARD_HEIGHT][BOARD_WIDTH]);
-
 #define DIR_UP 1
 #define DIR_DOWN 2
 #define DIR_LEFT 3
@@ -40,10 +37,7 @@ void board_insert_block(cell_t board[BOARD_HEIGHT][BOARD_WIDTH], block_t* block,
 
 void board_move_block(windows_console_t* console, cell_t board[BOARD_HEIGHT][BOARD_WIDTH], block_t* block, uint8_t dir);
 void board_roate_block(block_t* block);
-//void board_draw(windows_console_t* console, cell_t board[BOARD_HEIGHT][BOARD_WIDTH]);
 void board_insert_cell(cell_t board[BOARD_HEIGHT][BOARD_WIDTH], cell_t* cell, int cell_x, int cell_y);
-//void board_clear_data(cell_t board[BOARD_HEIGHT][BOARD_WIDTH]);
-//void board_insert_block(cell_t board[BOARD_HEIGHT][BOARD_WIDTH], block_t* block, int block_x, int block_y);
 
 extern cell_t gboard[BOARD_HEIGHT][BOARD_WIDTH]; // 다른 c 파일에서도 전역변수 사용하기 위해 extern
 extern cell_attributes_t board_data_attributes[BOARD_HEIGHT][BOARD_WIDTH]; // 다른 c 파일에서도 전역변수 사용하기 위해 extern
